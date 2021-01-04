2aee9283f076def6849a9d1e200b23b8ba0e6043
Azhar <azhar.basit@gmail.com>
2021-01-04 18:16:47 +0900

    Added a heapq based MaxHeap that takes in tuple elements and keeps thems orted on values

---
+++ MaxHeap.py
@@ -0,0 +1,20 @@
+from heapq import heapify, heappush, heappushpop, nlargest
+from typing import List
+
+# Class to convert heapq, which is a min heap into max heap with a fixed max size.
+# Heapq is not synchrnoized so only thread can update this
+class MaxHeap:
+    def __init__(self, list:List, x_size):
+        self.heap = [(element[1], element[0]) for element in list]
+        self.length = x_size
+        heapify( self.heap)
+    
+    # here tuple should be value, key
+    def add(self, element):
+        if len(self.heap) < self.length:
+            heappush(self.heap, element)
+        else:
+            heappushpop(self.heap, element)
+            
+    def getLargest(self):
+        return nlargest(self.length, self.heap)
