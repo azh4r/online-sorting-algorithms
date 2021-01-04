524015a567bd798ce6fe48536c9eeac7155d81ce
Azhar <azhar.basit@gmail.com>
2021-01-04 21:18:11 +0900

    Added different arguments to start the app using different algorithms

--- LargestValues.py
+++ LargestValues.py
@@ -9,6 +9,7 @@ from DataFile import DataFile
 import time
 import click
 import validators
+from pathlib import Path
 
 # This module reads in the command line parameters and 
 # has the class and functions to execute the main process. 
@@ -156,18 +157,54 @@ class LargestValues:
 def main(remote_file_url, x_largest_numbers,chunk_size_in_blocks):
     file_location = sys.argv[1] if len(sys.argv) >=2 else LargestValues.DEFAULT_FILE_LOCATION
     offset_bytes = 500
+    LargestValues.process(remote_file_url, chunk_size_in_blocks, offset_bytes, x_largest_numbers)
     LargestValues.process_maxheap(remote_file_url, chunk_size_in_blocks, offset_bytes, x_largest_numbers)
-    #LargestValues.process_with_local_files(file_location, X_largest_numbers)
+    LargestValues.process_with_local_files(file_location, x_largest_numbers)
 
-@click.command()
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+@click.group(context_settings=CONTEXT_SETTINGS, chain=True)
+def cli():
+    # App for get the x largest values from a key-value data source
+    # python LargestValues.py memory_merges
+    # python LargestValues.py single_priority_queue
+    # python LargestValues.py files_on_disk
+    pass
+
+@cli.command()
 @click.option('--url', default=LargestValues.REMOTE_FILE_LOCATION, type=click.STRING, help='URL location for the data file')
 @click.option('--x', default=LargestValues.DEFAULT_X, type=click.INT, help='Number of Largest Values to get from data file')
 @click.option('--chunk_size', default=LargestValues.CHUNK_SIZE_IN_BLOCKS, type=click.INT, help= 'Size of chunk to retrieve from remote file and process at a time in blocks of 1024 bytes.')
-def cli(url, x, chunk_size):
+def memory_merges(url, x, chunk_size):
     if not validators.url(url):
         print("Entered url did not pass validation, please make sure it is correct.")
         return
-    main(url, int(x), int(chunk_size))
+    offset_bytes = 500
+    LargestValues.process(url, int(chunk_size), offset_bytes, int(x))
+
+@cli.command()
+@click.option('--url', default=LargestValues.REMOTE_FILE_LOCATION, type=click.STRING, help='URL location for the data file')
+@click.option('--x', default=LargestValues.DEFAULT_X, type=click.INT, help='Number of Largest Values to get from data file')
+@click.option('--chunk_size', default=LargestValues.CHUNK_SIZE_IN_BLOCKS, type=click.INT, help= 'Size of chunk to retrieve from remote file and process at a time in blocks of 1024 bytes.')
+def single_priority_queue(url, x, chunk_size):
+    if not validators.url(url):
+        print("Entered url did not pass validation, please make sure it is correct.")
+        return
+    offset_bytes = 500
+    LargestValues.process_maxheap(url, int(chunk_size), offset_bytes, int(x))
+
+@cli.command()
+@click.option('--file', default=LargestValues.DEFAULT_FILE_LOCATION, type=click.STRING, help='File location for the data file')
+@click.option('--x', default=LargestValues.DEFAULT_X, type=click.INT, help='Number of Largest Values to get from data file')
+@click.option('--chunk_size', default=LargestValues.CHUNK_SIZE_IN_BLOCKS, type=click.INT, help= 'Size of chunk to retrieve from remote file and process at a time in blocks of 1024 bytes.')
+def files_on_disk(file, x, chunk_size):
+    print(file)
+    if not os.path.isfile(file):
+        print("File not found, please make sure it is correct.")
+        return
+    offset_bytes = 500
+    LargestValues.process_with_local_files(file, int(x))
+
 
 
 if __name__ == '__main__':
