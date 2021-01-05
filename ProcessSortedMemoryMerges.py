f7e96bbe6a7c7161e7895661386999b438ad0174
Azhar <azhar.basit@gmail.com>
2021-01-06 02:59:17 +0900

    Refactored code to support instantiating class with __init__ and self. Refactored callback code to support object passed back to reference instance vars

--- ProcessSortedMemoryMerges.py
+++ ProcessSortedMemoryMerges.py
@@ -11,34 +11,36 @@ import time
 class SortedMemoryMerge:
 
     progress_bar = None
-    x_largest_value = 0
+    # result_dict = {}
 
-    #def __init__(self):
-        #self.progress_bar = None
+    def __init__(self):
+        # not needed
+        self.x_largest_values = 0
+        self.result_dict = {}
 
     def process(self,remote_file_url, chunk_size, offset_bytes, X_largest_values):
         response_handle = FileDownloader.get_response_handle(remote_file_url, offset_bytes)        
-        self.x_largest_value = X_largest_values
-        self.get_chunks(self,response_handle, chunk_size, offset_bytes)
+        self.x_largest_values = X_largest_values
+        self.get_chunks(response_handle, chunk_size, offset_bytes)
 
     def get_chunks(self,response_handle, chunk_size, offset_bytes):
         file_size = int(response_handle.headers.get('content-length', 0))
         self.progress_bar = tqdm(total=file_size, unit='iB', unit_scale=True)
-        FileDownloader.get_chunks(response_handle, chunk_size, offset_bytes, self.process_chunk)
+        FileDownloader.get_chunks(self,response_handle, chunk_size, offset_bytes, self.process_chunk)
 
-    def process_chunk(lines, last_chunk, chunk_size):
-        SortedMemoryMerge.progress_bar.update(chunk_size)
+    def process_chunk(func_object,object,lines, last_chunk, chunk_size):
+        object.progress_bar.update(chunk_size)
         dict_chunk = dict_lines(lines)
         sorted_dict_chunk = sort_dict(dict_chunk)
-        result_generator = heapq.merge(sorted_dict_chunk.items(), LargestValues.result_dict.items(),
+        result_generator = heapq.merge(sorted_dict_chunk.items(), object.result_dict.items(),
             key = lambda item:item[1], reverse=True)
         # Saving only the first X items in merge sorted dict
-        sliced_generator = itertools.islice(result_generator, SortedMemoryMerge.x_largest_value)
-        LargestValues.result_dict = {c[0]:c[1] for c in sliced_generator}
+        sliced_generator = itertools.islice(result_generator, object.x_largest_values)
+        object.result_dict = {c[0]:c[1] for c in sliced_generator}
         #time.sleep(1)
         if last_chunk:
             # write the result
-            DataFile.write_file(LargestValues.result_dict, "result_final")
-            SortedMemoryMerge.progress_bar.close()
-            for k, v in LargestValues.result_dict.items():
+            DataFile.write_file(object.result_dict, "result_final")
+            object.progress_bar.close()
+            for k, v in object.result_dict.items():
                 print(k)
