e60eca4e7da4654e3a0c31bacd03d04c9df1d42d
Azhar <azhar.basit@gmail.com>
2021-01-06 02:54:11 +0900

    Refactored different processing code to outside of this main calling object

--- LargestValues.py
+++ LargestValues.py
@@ -1,10 +1,7 @@
 from MaxHeap import MaxHeap
 import sys
-import glob, os
-import heapq
-import itertools
+import os
 from tqdm import tqdm
-import FileDownloader
 from DataFile import DataFile
 import time
 import click
@@ -12,11 +9,10 @@ import validators
 from pathlib import Path
 import ProcessSortedMemoryMerges
 import ProcessSinglePriorityQueue
+import ProcessUsingLocalFileSortMerges
 
 # This module reads in the command line parameters and 
 # has the class and functions to execute the main process. 
-
-
 
 
 class LargestValues:
@@ -25,81 +21,21 @@ class LargestValues:
     REMOTE_FILE_LOCATION = 'https://amp-spacemaps-technical-challenge.s3-ap-northeast-1.amazonaws.com/spacemaps_technical_challenge.txt'
     CHUNK_SIZE_IN_BLOCKS = 8 # each block is 1024 byte
 
-    file_location = ''
-    result_dict = {}
-    X_largest_values = 0
-    max_heap = None
-
 
     def processSortedMemoryMerges(url, chunk_size, offset_bytes, x_largest_values):
-        sortedMemoryMerge = ProcessSortedMemoryMerges.SortedMemoryMerge
-        sortedMemoryMerge.process(sortedMemoryMerge,url, int(chunk_size), offset_bytes, int(x_largest_values))
+        sortedMemoryMerge = ProcessSortedMemoryMerges.SortedMemoryMerge()
+        sortedMemoryMerge.process(url, int(chunk_size), offset_bytes, int(x_largest_values))
 
 
     def processSinglePriorityQueueMerges(url, chunk_size, offset_bytes, x_largest_values):
-        singlePriorityQueueMerges = ProcessSinglePriorityQueue.SinglePriorityQueueMerges
-        singlePriorityQueueMerges.process_maxheap(singlePriorityQueueMerges, url, chunk_size, offset_bytes, x_largest_values)
+        singlePriorityQueueMerges = ProcessSinglePriorityQueue.SinglePriorityQueueMerges()
+        singlePriorityQueueMerges.process_maxheap( url, chunk_size, offset_bytes, x_largest_values)
 
 
-    # Function to sort merge n files but 2 files at a time. 
-    # Can do n file merge as heapq.merge can take more than 2 iterables
-    # but in case of limited RAM and very large X value merging 2 files is better. 
-    def sort_merge_files(X_largest_numbers):
-        print("sort merge files 2 at a time")
-        # read all outfile_# into separate dict
-            # scan all files with 'outfile_' prefix 
-            # not a good design, needs to be refactored.
-        out_directory = "./"
-        os.chdir(out_directory)
-        result_dict = {}
-        for file in glob.glob("outfile_*.txt"):
-            # read each of these files into a separate dict
-            temp_dict = {}
-            with open(file, 'r') as f:
-                for line in f:
-                    key, val = line.split()
-                    temp_dict[key] = int(val)
-            # use heapq.merge(iterables, key, reverse = True)  two dict at a time, this maybe better than doing an n-file merge sort in case
-            # of limited storage.  The merged files can then be deleted as we pull more file segements from remote.
-            # got stuck here as took me a while to figure out how to use dict with heapq.merge
-            result_generator = heapq.merge(temp_dict.items(), result_dict.items(), key = lambda item:item[1], reverse=True)
-            # Saving only the first X_largest_numbers items in merge sorted dict
-            sliced_generator = itertools.islice(result_generator, X_largest_numbers)
-            result_dict = {c[0]:c[1] for c in sliced_generator}
-
-        # write the result_dict dictionary
-        DataFile.write_file(result_dict, "result_final")
-
-    def process_with_local_files(self,file_location, X_largest_numbers):
-        # Get count of lines in file.. then calculate the number of lines per file read 
-        # OR amount of lines you want to read at a time <-- used this , ignore above
-        # create a loop that will keep calling read_file(), sort_dict(), write_file() until 
-        # end of file is reached.
-        end_of_file = False
-        offset = 0
-        # process the local file 500 lines at a time
-        lines_to_read = 500
-        file_handle = DataFile.get_handle(file_location)
-
-        # skip the first 500 bytes
-        file_dict, offset, end_of_file = DataFile.read_file(file_handle, 0, 500)
-        # Write the read file into sorted segmented file chunks
-        file_suffix = 1
-        # here this writes the same file_name prefix as to be read by sort_merge_files 
-        # not a good design, needs to be refactored. 
-        file_name_prefix = "outfile_"
-        while not end_of_file:
-            file_dict, offset, end_of_file = DataFile.read_file(file_handle, lines_to_read, offset)
-            sorted_dict = sort_dict(file_dict)
-            DataFile.write_file(sorted_dict, file_name_prefix + str(file_suffix))
-            file_suffix += 1
-
-        # do n sort file merge
-        # get the top X numbers from the final file
-        self.sort_merge_files(X_largest_numbers)
-
+    def processUsingLocalFileMerges(file_name, x_largest_values):
+        localFileSortMerges = ProcessUsingLocalFileSortMerges.LocalFileSortMerges()
+        localFileSortMerges.process_with_local_files( file_name, x_largest_values)
     
-
 
 # Read in the file name from command line
 # parameters must be X, location of file.   (these 2 are required)
@@ -154,7 +90,7 @@ def files_on_disk(file, x, chunk_size):
         print("File not found, please make sure it is correct.")
         return
     offset_bytes = 500
-    LargestValues.process_with_local_files(file, int(x))
+    LargestValues.processUsingLocalFileMerges(file, int(x))
 
 
 
