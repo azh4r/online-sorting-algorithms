ea115c815db91a06758c7d392c43eb1d8a4f4782
Azhar <azhar.basit@gmail.com>
2021-01-06 02:56:17 +0900

    Refactored to handle callback method accessing instance variables

--- ProcessSinglePriorityQueue.py
+++ ProcessSinglePriorityQueue.py
@@ -12,17 +12,17 @@ class SinglePriorityQueueMerges:
         response_handle = FileDownloader.get_response_handle(remote_file_url, offset_bytes)
         file_size = int(response_handle.headers.get('content-length', 0))
         self.progress_bar = tqdm(total=file_size, unit='iB', unit_scale=True)
-        FileDownloader.get_chunks(response_handle, chunk_size, offset_bytes, SinglePriorityQueueMerges.process_single_maxheap)
+        FileDownloader.get_chunks(self,response_handle, chunk_size, offset_bytes, self.process_single_maxheap)
 
     # Callback function
-    def process_single_maxheap(lines, last_chunk, chunk_size):
-        SinglePriorityQueueMerges.progress_bar.update(chunk_size)
+    def process_single_maxheap(object_function,object,lines, last_chunk, chunk_size):
+        object.progress_bar.update(chunk_size)
         for line in lines:
             key, value = line.split()
-            SinglePriorityQueueMerges.max_heap.add((int(value),key))
+            object.max_heap.add((int(value),key))
         if last_chunk:
-            SinglePriorityQueueMerges.progress_bar.close()
+            object.progress_bar.close()
             # write the result
-            elements = SinglePriorityQueueMerges.max_heap.getLargest()
+            elements = object.max_heap.getLargest()
             for elem in elements:
                 print(elem[1],' ', elem[0])
