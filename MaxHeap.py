38a36e08aeeadade41809c5b334246d00c3ed21f
Azhar <azhar.basit@gmail.com>
2021-01-07 20:23:27 +0900

    MaxHeap.py : refactored name of class to FixedSizeMaxHeap as that is more correct.

--- MaxHeap.py
+++ MaxHeap.py
@@ -1,10 +1,10 @@
 from heapq import heapify, heappush, heappushpop, nlargest
 
 # Class to keep a heap with a fixed max size using heapq. 
-# this will be a min heap but since we only ask for nlargest values once at the end this 
-# will work for our specific usecase. 
+# this is a min heap but since we only ask for nlargest values once at the end this 
+# will function as a MaxHeap for our specific usecase. 
 # Heapq is not synchrnoized so only thread can update this
-class MaxHeap:
+class FixedSizeMaxHeap:
     def __init__(self, list, x_size):
         self.heap = [(element[1], element[0]) for element in list]
         self.length = x_size
@@ -16,6 +16,7 @@ class MaxHeap:
             heappush(self.heap, element)
         else:
             heappushpop(self.heap, element)
-            
+    
+    # 
     def getValues(self):
         return nlargest(self.length, self.heap)
