068e5208bcd4c0283d9bd0fc56f118fceddd3bad
Azhar <azhar.basit@gmail.com>
2021-01-07 21:05:53 +0900

    commented writing of results file

--- ProcessUsingLocalFileSortMerges.py
+++ ProcessUsingLocalFileSortMerges.py
@@ -39,8 +39,8 @@ class LocalFileSortMerges:
 
         # write the result_dict dictionary
         result_file = "result_final"
-        DataFile.write_file(result_dict, result_file)
-        for k,v in result_dict.items():
+        #DataFile.write_file(result_dict, result_file)
+        for k,v in result_dict.items.items():
             print(k)
 
 
